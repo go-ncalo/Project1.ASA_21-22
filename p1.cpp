6837b29c0ca056f96df21df926eaf01a7f5e20cb
go-ncalo <goncalo@LAPTOP-IBVIO7VS.localdomain>
2021-12-30 22:51:51 -0100

    both exercises done, needs to better memory usage (1350 on mooshak)

--- p1.cpp
+++ p1.cpp
@@ -58,18 +58,56 @@ vector<int> lis(vector<int> v) {
     return {max, count};
 }
 
+int lics(vector<int> v1, vector<int> v2) {
+    size_t i, j, n = v1.size(), m = v2.size();
+    vector<vector<int>> s(n+1, vector<int>(m+1, 0));
+    
+    for (i = 1; i <= n; i++) {
+        int current_sequence = 0;
+        for (j = 1; j <= m; j++) {
+            if (v1[i-1] == v2[j-1]) {
+                if (current_sequence + 1 > s[i-1][j]) {
+                    s[i][j] = current_sequence + 1;
+                } else {
+                    s[i][j] = s[i-1][j];
+                }
+            } else if (v1[i-1] > v2[j-1]) {
+                if (s[i-1][j] > current_sequence) {
+                    current_sequence = s[i-1][j];
+                }
+                s[i][j] = s[i-1][j];
+            } else {
+                s[i][j] = s[i-1][j];
+            }
+        }
+    }
+    int length = 0;
+    i = n;
+    for (j = 0; j <= m; j++) {
+        if (s[i][j] > length)
+            length = s[i][j];
+    }
+    return length;
+}
+
 int main() {
     int i;
     string input;
     vector<vector<int>> intSequence;
-    vector<int> result;
 
     cin >> i;
     intSequence = getIntSequence(i);
     
     if (i == 1) {
+        vector<int> result;
         result = lis(intSequence[0]);
         cout << result[0] << ' ' << result[1] << endl;
+    }
+
+    if (i == 2) {
+        int result;
+        result = lics(intSequence[0], intSequence[1]);
+        cout << result << endl;
     }
 
     return 0;
